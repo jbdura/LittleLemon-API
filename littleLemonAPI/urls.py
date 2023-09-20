cd45d83fde44e2e311607f31884220457e0cbbd1
jbdura <bidurajohnson@gmail.com>
2023-09-20 11:09:08 +0300

    created urls

--- littleLemonAPI/urls.py
+++ littleLemonAPI/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from restaurant.views import BookingViewSet
+from restaurant import views
+
+router = routers.DefaultRouter()
+router.register(r'booking', BookingViewSet)
+
+urlpatterns = router.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
