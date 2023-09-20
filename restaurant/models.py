a48b02c15b8650b25ccc6325449fb469fbba5fe2
jbdura <bidurajohnson@gmail.com>
2023-09-20 11:10:30 +0300

    added models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField(validators=[MinValueValidator(1)])
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(validators=[MaxValueValidator(5)])
+
+    def __str__(self):
+        return f'{self.title}: {str(self.price)}'
