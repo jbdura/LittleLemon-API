335d1bf240291ad456f6e8fb88acac2f858dfacd
jbdura <bidurajohnson@gmail.com>
2023-09-20 11:11:23 +0300

    made migrations and added static files

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,12 @@
-#define URL route for index() view
+from django.contrib import admin
 from django.urls import path
 from . import views
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
+    path('message/', views.msg),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Menu
+from rest_framework import viewsets
+from django.http import HttpResponse
+from .models import Booking
+from .serializers import BookingSerializer
+from .serializers import MenuSerializer
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+)
+from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message": "This view is protected"})
+
+
+class MenuView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    pass
+
+
+class SingleMenuView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    pass
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    pass
