078c047846f5a66cc353f9fc5877f03b5820e9de
jbdura <bidurajohnson@gmail.com>
2023-09-24 14:14:27 +0300

    Touch up

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -3,7 +3,9 @@ from django.core.exceptions import ValidationError
 from .models import Booking, Menu
 
 class BookingModelTest(TestCase):
-
+    """
+    tests Booking models
+    """
     def test_booking_invalid_guests(self):
         # Create a Booking instance with invalid no_of_guests value
         booking = Booking(name="Test Booking", no_of_guests=0)
@@ -12,7 +14,9 @@ class BookingModelTest(TestCase):
             booking.full_clean()
 
 class MenuModelTest(TestCase):
-
+    """
+    tests Menu models
+    """
     def test_menu_invalid_inventory(self):
         # Create a Menu instance with invalid inventory value
         menu = Menu(title="Test Menu", price=10.99, inventory=10)  # Invalid inventory value
